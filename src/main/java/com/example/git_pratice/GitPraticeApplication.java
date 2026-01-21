b3468651ba5fd37863689c46fc19312a499c2a08
soheb <sohebkhan3145@gmail.com>
2026-01-21 16:47:00 +0530

    Initial commit

---
+++ src/main/java/com/example/git_pratice/GitPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPraticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git_pratice/GitPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
